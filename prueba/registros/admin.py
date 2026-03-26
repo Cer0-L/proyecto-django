5eb2f788a8e31095d595a4264e6799b8e7549234
Cero-L <amaranthacruz64@gmail.com>
2026-03-26 11:50:25 -0600

    Primer envío de archivos

---
+++ prueba/registros/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from .models import Alumnos
+from .models import Comentario
+from .models import ComentarioContacto
+
+class AdministrarModelo(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('matricula', 'nombre', 'carrera', 'turno','created')
+    search_fields = ('matricula', 'nombre', 'carrera', 'turno')
+    date_hierarchy = 'created'
+    list_filter = ('carrera', 'turno')
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name='usuarios').exists():
+            return ('created', 'updated', 'matricula', 'carrera', 'turno')
+        else:
+            return ('created', 'updated')
+        
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name='permisos').exists():
+            return ('matricula', 'turno')
+        else:
+            return ('created', 'updated')
+
+admin.site.register(Alumnos, AdministrarModelo)
+
+class AdministrarComentarios(admin.ModelAdmin):
+    list_display = ('id', 'coment')
+    search_fields = ('id', 'coment')
+    date_hierarchy = 'created'
+    readonly_fields = ('created', 'id')
+
+admin.site.register(Comentario, AdministrarComentarios)
+
+
+class AdministrarComentariosContacto(admin.ModelAdmin):
+    list_display = ('id', 'mensaje')
+    search_fields = ('id','created')
+    date_hierarchy = 'created'
+    readonly_fields = ('created', 'id')
+
+admin.site.register(ComentarioContacto, AdministrarComentariosContacto)
